ff1f83437c8ba7de4880ef873578ba5f4771d021
tastymushroom <54619692+tastymushroom@users.noreply.github.com>
2020-08-28 18:02:50 +0200

    Update install.sh
    Update to create passwords automatically during install.

--- docs/Rover/install.sh
+++ docs/Rover/install.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
 # Create user romi
-sudo adduser romi
+# sudo adduser romi
+# sudo adduser romi dialout
+# sudo adduser romi video
+# sudo adduser romi sudo
+
+# Install the dependencies
+# sudo apt install build-essential cmake git libpng-dev libjpeg9-dev
+
+# Download, compile and install the libraries & apps
+# git clone https://github.com/romi/romi-rover-build-and-test.git
+# cd romi-rover-build-and-test
+# mkdir -p build
+# cd build
+# cmake cmake -DCMAKE_INSTALL_PREFIX=~/bin .. 
+# make
+
+sudo useradd -m -s /bin/bash -G sudo romi
+sudo echo "p2pfoodlab\np2pfoodlab" | passwd romi
 sudo adduser romi dialout
 sudo adduser romi video
 sudo adduser romi sudo
-
-# Install the dependencies
-sudo apt install build-essential cmake git libpng-dev libjpeg9-dev
-
-
-
-# Download, compile and install the libraries & apps
-git clone https://github.com/romi/romi-rover-build-and-test.git
+sudo echo p2pfoodlab | su - romi
+git clone --branch ci_dev --recurse-submodules https://github.com/romi/romi-rover-build-and-test.git
 cd romi-rover-build-and-test
 mkdir -p build
 cd build
-cmake cmake -DCMAKE_INSTALL_PREFIX=~/bin .. 
+cmake -DCMAKE_INSTALL_PREFIX=~/romi-rover .. 
 make
