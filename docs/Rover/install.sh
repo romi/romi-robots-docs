456c37e5c0a319170aab10ed690d9c2b68b3a177
Doug Boari <doug@tastymushroom.com>
2020-09-01 11:21:36 +0200

    Revert install file to opriginal version, add readme output to new romi account.

--- docs/Rover/install.sh
+++ docs/Rover/install.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
 
 # Create user romi
-# sudo adduser romi
-# sudo adduser romi dialout
-# sudo adduser romi video
-# sudo adduser romi sudo
-
-# Install the dependencies
-# sudo apt install build-essential cmake git libpng-dev libjpeg9-dev
-
-# Download, compile and install the libraries & apps
-# git clone https://github.com/romi/romi-rover-build-and-test.git
-# cd romi-rover-build-and-test
-# mkdir -p build
-# cd build
-# cmake cmake -DCMAKE_INSTALL_PREFIX=~/bin .. 
-# make
-
-sudo useradd -m -s /bin/bash -G sudo romi
-sudo echo -e "p2pfoodlab\np2pfoodlab" | sudo passwd romi
+sudo adduser romi
 sudo adduser romi dialout
 sudo adduser romi video
-sudo echo p2pfoodlab | su - romi
+sudo adduser romi sudo
+
+# Install the dependencies
+sudo apt install build-essential cmake git libpng-dev libjpeg9-dev
+
+echo "
+Readme.romi.txt
+To clone the romi rover build and test development project:
 git clone --branch ci_dev --recurse-submodules https://github.com/romi/romi-rover-build-and-test.git
-cd romi-rover-build-and-test
-mkdir -p build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=~/romi-rover .. 
-make -j4
+
+See: https://github.com/romi/romi-rover-build-and-test/ReadMe.md
+" >> /home/romi/ReadMe.romi.txt
+
+chmod a+r /home/romi/ReadMe.romi.txt
+
